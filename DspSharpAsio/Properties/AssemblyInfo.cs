abc142a3443639fcb28814273297d1b0bde87298
Jonathan Arweck <jonathanarweck@freenet.de>
2017-07-27 20:59:11 +0200

    Update DspSharpAsio to new csproj

--- DspSharpAsio/Properties/AssemblyInfo.cs
+++ DspSharpAsio/Properties/AssemblyInfo.cs
@@ -4,21 +4,7 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("DspSharpAsio")]
-[assembly: AssemblyDescription("ASIO Input/Output for DspSharp")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DspSharpAsio")]
-[assembly: AssemblyCopyright("Copyright © 2017 Jonathan Arweck")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -30,17 +16,3 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("8420c3e2-ca65-44a1-bbc7-f259751fae9c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-pre")]
